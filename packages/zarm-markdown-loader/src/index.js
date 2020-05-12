6c2b6306a6e6768a094d94b58e8e1ba8cfee6421
Jerome Lin <jerome.lin@zhongan.com>
2020-05-12 18:43:36 +0800

    chore: update markdown-loader

--- packages/zarm-markdown-loader/src/index.js
+++ packages/zarm-markdown-loader/src/index.js
@@ -1,12 +1,12 @@
 
 const MarkTwain = require('mark-twain');
-const toReactElement = require('jsonml-to-react-element');
+// const toReactElement = require('jsonml-to-react-element');
 
 module.exports = (source) => {
   const { meta, content } = MarkTwain(source);
   return {
     meta,
     content,
-    component: toReactElement(content),
+    // component: toReactElement(content),
   };
 };
