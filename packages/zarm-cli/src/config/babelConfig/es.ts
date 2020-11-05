255504f2c20cd158a422da7c385da7994e134c62
Jerome Lin <jerome.lin@zhongan.com>
2020-11-05 19:05:34 +0800

    perf: add babel runtime plugin

--- packages/zarm-cli/src/config/babelConfig/es.ts
+++ packages/zarm-cli/src/config/babelConfig/es.ts
@@ -7,6 +7,7 @@ export default {
     require.resolve('@babel/preset-typescript'),
   ],
   plugins: [
+    require.resolve('@babel/plugin-transform-runtime'),
     [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
     [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
     require.resolve('@babel/plugin-proposal-optional-chaining'),
--- packages/zarm-cli/src/config/babelConfig/lib.ts
+++ packages/zarm-cli/src/config/babelConfig/lib.ts
@@ -5,6 +5,7 @@ export default {
     require.resolve('@babel/preset-typescript'),
   ],
   plugins: [
+    require.resolve('@babel/plugin-transform-runtime'),
     [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
     [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
     require.resolve('@babel/plugin-proposal-optional-chaining'),
