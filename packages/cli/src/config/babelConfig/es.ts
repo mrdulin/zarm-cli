133acfac653d4cad540e8c7c8d65fcaca4f09095
Jerome Lin <jerome.lin@zhongan.com>
2021-02-04 14:31:04 +0800

    chore(babel): remove core-js3

--- packages/cli/src/config/babelConfig/es.ts
+++ packages/cli/src/config/babelConfig/es.ts
@@ -1,15 +1,3 @@
-export default {
-  presets: [
-    [require.resolve('@babel/preset-env'), {
-      modules: false,
-    }],
-    require.resolve('@babel/preset-react'),
-    require.resolve('@babel/preset-typescript'),
-  ],
-  plugins: [
-    [require.resolve('@babel/plugin-transform-runtime'), { corejs: 3 }],
-    [require.resolve('@babel/plugin-proposal-decorators'), { legacy: true }],
-    [require.resolve('@babel/plugin-proposal-class-properties'), { loose: true }],
-    require.resolve('@babel/plugin-proposal-optional-chaining'),
-  ],
-};
+import BaseConfig from './base';
+
+export default BaseConfig;
