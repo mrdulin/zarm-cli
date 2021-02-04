4c1759b9d8fc50736a02e5a77ce38a48720c12ef
mrdulin <novaline.dulin@gmail.com>
2021-02-04 18:40:55 +0800

    test: add unit tests for deploy.ts

--- packages/cli/src/deploy.ts
+++ packages/cli/src/deploy.ts
@@ -16,10 +16,11 @@ export interface IDeployConfig {
   analyzer: boolean;
 }
 
-export const getProjectConfig = (config: Configuration) => {
+export function getProjectConfig(config: Configuration) {
   const { entries, setBabelOptions, banner, setRules, setPlugins, ...webpackConfig } = getCustomConfig();
 
   config.entry = {};
+  config.plugins = config.plugins || [];
   setBabelOptions && setBabelOptions(config.module.rules[0].use[0].options);
   setRules && setRules(config.module.rules);
   setPlugins && setPlugins(config.plugins);
@@ -28,19 +29,19 @@ export const getProjectConfig = (config: Configuration) => {
     if (entries[key].entry) {
       config.entry[key] = entries[key].entry;
     }
-    config.plugins.push(
-      new HtmlWebpackPlugin({
-        template: entries[key].template,
-        filename: `${key}.html`,
-        chunks: ['manifest', key],
-        favicon: entries[key].favicon,
-        inject: entries[key].inject !== false,
-      }),
-    );
+    const htmlWebpackPlugin = new HtmlWebpackPlugin({
+      template: entries[key].template,
+      filename: `${key}.html`,
+      chunks: ['manifest', key],
+      favicon: entries[key].favicon,
+      inject: entries[key].inject !== false,
+    });
+
+    config.plugins.push(htmlWebpackPlugin);
   });
 
   return webpackMerge(config, webpackConfig);
-};
+}
 
 export default ({ outDir, pushGh, analyzer }: IDeployConfig) => {
   const config = getProjectConfig(getWebpackConfig('deploy'));
