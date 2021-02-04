caafc368346a8843e9515b2de0b2c30ac6088edb
mrdulin <novaline.dulin@gmail.com>
2021-02-04 15:28:18 +0800

    test: improve unit test for template.ts

--- packages/cli/src/__tests__/template.spec.ts
+++ packages/cli/src/__tests__/template.spec.ts
@@ -2,11 +2,16 @@ import { mocked } from 'ts-jest/utils';
 import fs from 'fs';
 import { sync } from 'mkdirp';
 import signale from 'signale';
+import chalk from 'chalk';
 import createTemplate from '../template';
 
 jest.mock('mkdirp');
+jest.mock('chalk', () => {
+  return { green: jest.fn() };
+});
 
 const syncMocked = mocked(sync);
+const chalkGreenMocked = mocked(chalk.green);
 
 describe('template', () => {
   afterAll(() => {
@@ -18,6 +23,7 @@ describe('template', () => {
   });
   describe('#createTemplate', () => {
     it('should create react component code snippet', () => {
+      chalkGreenMocked.mockImplementation((str: any) => str);
       const openSyncMocked = jest.spyOn(fs, 'openSync').mockReturnValue(1);
       const writeSyncMocked = jest.spyOn(fs, 'writeSync').mockReturnValue(1);
       const infoStub = jest.spyOn(console, 'info').mockReturnValue();
@@ -27,12 +33,14 @@ describe('template', () => {
       expect(syncMocked).toBeCalledWith('components/primary-button');
       expect(syncMocked).toBeCalledWith('components/primary-button/style');
       expect(syncMocked).toBeCalledWith('components/primary-button/__tests__');
-      expect(infoStub).toBeCalledWith(expect.stringMatching('components/primary-button/index.tsx'));
-      expect(infoStub).toBeCalledWith(expect.stringMatching('components/primary-button/demo.md'));
-      expect(infoStub).toBeCalledWith(expect.stringMatching('components/primary-button/primary button.tsx'));
-      expect(infoStub).toBeCalledWith(expect.stringMatching('components/primary-button/style/index.tsx'));
-      expect(infoStub).toBeCalledWith(expect.stringMatching('components/primary-button/style/index.scss'));
-      expect(infoStub).toBeCalledWith(expect.stringMatching('components/primary-button/__tests__/index.test.jsx'));
+      expect(infoStub).toBeCalledWith(expect.stringMatching('create components/primary-button/index.tsx'));
+      expect(infoStub).toBeCalledWith(expect.stringMatching('create components/primary-button/demo.md'));
+      expect(infoStub).toBeCalledWith(expect.stringMatching('create components/primary-button/primary button.tsx'));
+      expect(infoStub).toBeCalledWith(expect.stringMatching('create components/primary-button/style/index.tsx'));
+      expect(infoStub).toBeCalledWith(expect.stringMatching('create components/primary-button/style/index.scss'));
+      expect(infoStub).toBeCalledWith(
+        expect.stringMatching('create components/primary-button/__tests__/index.test.jsx'),
+      );
       expect(infoStub).toBeCalledTimes(6);
       expect(openSyncMocked).toBeCalledTimes(6);
       expect(writeSyncMocked).toBeCalledTimes(6);
